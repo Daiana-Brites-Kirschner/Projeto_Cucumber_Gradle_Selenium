584f1dfe73aae2a1bc26ca460321628df07a6fdb
Daiana-Brites-Kirschner <daia.brites@gmail.com>
2020-11-04 13:28:00 -0300

    Desenvolvimento de cenários utilizando Cucumber

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,19 @@
+package runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features/AprenderCucumber.feature",
+        glue = "steps",
+        tags = "@esse",
+        plugin = "pretty",
+        monochrome = true,
+        snippets = SnippetType.CAMELCASE,
+        dryRun = false,
+        strict = false
+)
+public class Runner {
+}
