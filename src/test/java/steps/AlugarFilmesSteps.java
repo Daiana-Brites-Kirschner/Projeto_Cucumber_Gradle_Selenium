0c01ae2df28228b79b444ae17925856ee3037690
Daiana-Brites-Kirschner <daia.brites@gmail.com>
2020-11-05 22:03:38 -0300

    Acrescentado mais cenários

--- src/test/java/steps/AlugarFilmesSteps.java
+++ src/test/java/steps/AlugarFilmesSteps.java
@@ -7,7 +7,10 @@ import entidades.Filme;
 import entidades.NotaAluguel;
 import org.junit.Assert;
 import service.AluguelService;
+import utils.DateUtils;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -16,8 +19,10 @@ public class AlugarFilmesSteps {
     private Filme filme;
     private AluguelService aluguel =new AluguelService();
     private NotaAluguel nota;
+    private String erro;
+    private String tipoAluguel;
 
-    @Dado("^um filme com estoque de (\\d+)unidades$")
+    @Dado("^um filme com estoque de (\\d+) unidades$")
     public void um_filme_com_estoque_de_unidades(int arg1) throws Throwable {
         filme = new Filme();
         filme.setEstoque(arg1);
@@ -30,7 +35,11 @@ public class AlugarFilmesSteps {
 
     @Quando("^alugar$")
     public void alugar() throws Throwable {
-        nota = aluguel.alugar(filme);
+        try {
+            nota = aluguel.alugar(filme, tipoAluguel);
+        } catch (RuntimeException e) {
+            erro = e.getMessage();
+        }
     }
 
     @Entao("^o preço do aluguel será R\\$ (\\d+)$")
@@ -39,23 +48,48 @@ public class AlugarFilmesSteps {
 
     }
 
-    @Entao("^a data de entrega será no dia seguinte$")
-    public void a_data_de_entrega_será_no_dia_seguinte() throws Throwable {
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DAY_OF_MONTH,1);
-
-        Date dataRetorno = nota.getDataEntrega();
-        Calendar calRetorno = Calendar.getInstance();
-        calRetorno.setTime(dataRetorno);
-
-        Assert.assertEquals(cal.get(Calendar.DAY_OF_MONTH),calRetorno.get(Calendar.DAY_OF_MONTH));
-        Assert.assertEquals(cal.get(Calendar.MONTH),calRetorno.get(Calendar.MONTH));
-        Assert.assertEquals(cal.get(Calendar.YEAR),calRetorno.get(Calendar.YEAR));
-    }
+//    @Entao("^a data de entrega será no dia seguinte$")
+//    public void a_data_de_entrega_será_no_dia_seguinte() throws Throwable {
+//        Calendar cal = Calendar.getInstance();
+//        cal.add(Calendar.DAY_OF_MONTH,1);
+//
+//        Date dataRetorno = nota.getDataEntrega();
+//        Calendar calRetorno = Calendar.getInstance();
+//        calRetorno.setTime(dataRetorno);
+//
+//        Assert.assertEquals(cal.get(Calendar.DAY_OF_MONTH), calRetorno.get(Calendar.DAY_OF_MONTH));
+//        Assert.assertEquals(cal.get(Calendar.MONTH), calRetorno.get(Calendar.MONTH));
+//        Assert.assertEquals(cal.get(Calendar.YEAR), calRetorno.get(Calendar.YEAR));
+//    }
 
     @Entao("^o estoque do filme será (\\d+) unidade$")
-    public void o_estoque_do_filme_será_unidade(int arg1) throws Throwable {
+    public void oEstoqueDoFilmeSeráUnidade(int arg1) throws Throwable {
         Assert.assertEquals(arg1,filme.getEstoque());
+    }
+
+    @Entao("^não será possível por falta de estoque$")
+    public void nãoSeráPossívelPorFaltaDeEstoque() throws Throwable {
+        Assert.assertEquals("Filme sem estoque",erro);
+    }
+
+    @Dado("^que o tipo do aluguel seja (.*)$")
+    public void queOTipoDoAluguelSejaExtendido(String tipo) throws Throwable {
+        tipoAluguel = tipo;
+    }
+
+    @Entao("^a data de entrega será em (\\d+) dias?$")
+    public void aDataDeEntregaSeráEmDias(int arg1) throws Throwable {
+        Date dataEsperada = DateUtils.obterDataComDiferencaDias(arg1);
+        Date dataReal = nota.getDataEntrega();
+
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+
+        Assert.assertEquals(format.format(dataEsperada),format.format(dataReal));
+    }
+
+    @Entao("^a pontuação recebida será de (\\d+) pontos$")
+    public void aPontuaçãoRecebidaSeráDePontos(int arg1) throws Throwable {
+    Assert.assertEquals(arg1,nota.getPontuacao());
     }
 
 }
--- src/test/resources/features/alugar_filme.feature
+++ src/test/resources/features/alugar_filme.feature
@@ -1,14 +1,38 @@
 # language: pt
-  @esse
+@esse
   Funcionalidade: Alugar filme
     Como um usuário
     Eu quero cadastrar alugueis de filmes
     Para controlar preços e datas de entrega
 
   Cenario: Deve alugar um filme com sucesso
-    Dado um filme com estoque de 2unidades
+    Dado um filme com estoque de 2 unidades
     E que o preço de aluguel seja R$3
     Quando alugar
     Entao o preço do aluguel será R$ 3
-    E a data de entrega será no dia seguinte
+    E a data de entrega será em 1 dia
     E o estoque do filme será 1 unidade
+
+    Cenario: Não deve alugar um filme sem estoque
+      Dado um filme com estoque de 0 unidades
+      Quando alugar
+      Entao não será possível por falta de estoque
+      E o estoque do filme será 0 unidade
+
+    Cenario: Deve dar condições especiais para categoria extendida
+      Dado um filme com estoque de 2 unidades
+      E que o preço de aluguel seja R$4
+      E que o tipo do aluguel seja extendido
+      Quando alugar
+      Entao o preço do aluguel será R$ 8
+      E a data de entrega será em 3 dias
+      E a pontuação recebida será de 2 pontos
+
+    Cenario: Deve alugar para categoria comum
+      Dado um filme com estoque de 2 unidades
+      E que o preço de aluguel seja R$4
+      E que o tipo do aluguel seja comum
+      Quando alugar
+      Entao o preço do aluguel será R$ 4
+      E a data de entrega será em 1 dia
+      E a pontuação recebida será de 1 pontos
